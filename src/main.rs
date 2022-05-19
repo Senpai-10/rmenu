491c9e029e33c7e135326331cee527b4a34766da
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-19 05:08:34 +0300

    change list

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,12 @@ mod menu;
 use menu::Menu;
 
 fn main() {
-    let list = vec![String::from("yes"), String::from("no")];
+    let list = vec![
+        String::from("1"),
+        String::from("2"),
+        String::from("3"),
+        String::from("4"),
+    ];
 
     let mut menu = Menu::new(list);
     menu.start();
